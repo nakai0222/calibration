10798b04054f42a3a7bc5426befb36a3849f40c5
nakai0222 <nanacololabi@gmail.com>
2015-06-12 00:41:43 +0900

    modified

--- calibration/opencv_threshold_test/test.cpp
+++ calibration/opencv_threshold_test/test.cpp
@@ -71,11 +71,11 @@ int main(){
 			if( (int)pixel >=  max[0] ){
 
 
-				for(int i=POINTNUM-1 ;i >= 1;i--){
-					max[i] = max[i-1];	
+				for(int k=POINTNUM-1 ;k >= 1;k--){
+					max[k] = max[i-1];	
 
-					max_num_i[i] = max_num_i[i-1];
-					max_num_j[i] = max_num_j[i-1];
+					max_num_i[k] = max_num_i[k-1];
+					max_num_j[k] = max_num_j[k-1];
 
 				}
 				max[0] = pixel;
@@ -130,24 +130,6 @@ int main(){
 	cv::namedWindow("R3");
 	imshow("R3",gimgl);
 
-
-	/*	
-		std::cout << max << std::endl;
-		std::cout << max_num << std::endl;
-		std::cout << max_num/gimgl.cols << std::endl;
-		std::cout << max_num%gimgl.cols << std::endl;
-		divide(split_imgl[(BGR+1)%3],split_imgl[BGR],gimgl2,10);
-		threshold(gimgl2,gimgl2,8,255,THRESH_BINARY_INV);
-		namedWindow("B/R");
-		imshow("B/R",gimgl2);
-		divide(split_imgl[(BGR+2)%3],split_imgl[BGR],gimgl3,10);
-		threshold(gimgl3,gimgl3,9,255,THRESH_BINARY_INV);
-		namedWindow("G/R");
-		imshow("G/R",gimgl3);
-		gimgl=gimgl.mul(gimgl2,1.0/255);
-		gimgl=gimgl.mul(gimgl3,1.0/255);
-		imshow("R2",gimgl);
-	 */
 	cv::waitKey(0);
 
 }
