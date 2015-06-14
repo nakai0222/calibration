9d7383709de0b028a96591a475df1055e32e12d3
nakai0222 <nanacololabi@gmail.com>
2015-06-14 22:33:51 +0900

    complete

--- calibration/estimate-plane-equation/test.cpp
+++ calibration/estimate-plane-equation/test.cpp
@@ -7,8 +7,8 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <math.h>
-
+//#include <math.h>
+#include <cmath>
 
 int main( int argc, char* argv[])
 {
@@ -18,9 +18,6 @@ int main( int argc, char* argv[])
 	cv::Point3f b (0,-2,1);
 	cv::Point3f c (3,-1,0);
 
-	std::cout << "a.x: " << a.x<< std::endl;
-	std::cout << "a.y: " << a.y<< std::endl;
-	std::cout << "a.z: " << a.z<< std::endl;
 	int x_sum = 0;
 	int y_sum = 0;
 	int z_sum = 0;
@@ -28,49 +25,48 @@ int main( int argc, char* argv[])
 	y_sum = a.y+b.y+c.y;
 	z_sum = a.z+b.z+c.z;
 
+	int x_y_sum = a.x*a.y+b.x*b.y+c.x*c.y;
+	int x_z_sum = a.x*a.z+b.x*b.z+c.x*c.z;
+	int y_z_sum = a.y*a.z+b.y*b.z+c.y*c.z;
+	
+	int x_sum_2 = a.x*a.x+b.x*b.x+c.x*c.x;		
+	int y_sum_2 = a.y*a.y+b.y*b.y+c.y*c.y;		
+	int z_sum_2 = a.z*a.z+b.z*b.z+c.z*c.z;		
+
 	std::cout << "x_sum : " << x_sum<< std::endl;
 	std::cout << "y_sum : " << y_sum<< std::endl;
 	std::cout << "z_sum : " << z_sum<< std::endl;
 
+	cv::Mat M = (cv::Mat_<double>(3,3) << x_sum_2,x_y_sum,x_sum,x_y_sum,y_sum_2,y_sum,x_sum,y_sum,3);
+	//cv::Mat M = (cv::Mat_<double>(3,3) << 3 ,x_sum,y_sum,x_sum,x_sum_2,x_sum*y_sum,y_sum,x_sum*y_sum,y_sum_2);
+
+	cv::Mat u = (cv::Mat_<double>(3,1)<< x_z_sum,y_z_sum, z_sum);
+	//cv::Mat u = (cv::Mat_<double>(3,1)<< z_sum ,x_sum*z_sum , y_sum*z_sum);
 
 
-	double a_t = x_sum*x_sum;	
-	double b_t = y_sum*y_sum;	
-	double c_t = x_sum*y_sum;	
-	double d_t = x_sum*z_sum;	
-	double e_t = y_sum*z_sum;	
-	double f_t = x_sum;	
-	double g_t = y_sum;	
-	double h_t = z_sum;
-	double n = 3;	
-
-
-
-	double ue =  (c_t*(g_t*h_t - n*e_t)+ f_t*(e_t*g_t - b_t*h_t) + d_t*(n*b_t - g_t*g_t) );
-	double sita = a_t*(g_t*g_t - n*b_t)-2*c_t*f_t*g_t +b_t*f_t*f_t + n*c_t*c_t ;
-	double a_v = - (ue/sita);
-
-	std::cout << "ue: "<< ue<< std::endl;	
-	std::cout << "sita: "<<sita<< std::endl;	
-	std::cout << "a_v: "<< a_v << std::endl;	
-	//cv::Mat M = (cv::Mat_<double>(3,3) << 16,x_sum*y_sum,x_sum,x_sum*y_sum,y_sum*y_sum,y_sum,x_sum,y_sum,3);
-	cv::Mat M = (cv::Mat_<double>(3,3) << 3 ,x_sum,y_sum,x_sum,x_sum*x_sum,x_sum*y_sum,y_sum,x_sum*y_sum,y_sum*y_sum);
-
-	//cv::Mat u = (cv::Mat_<double>(3,1)<< x_sum*z_sum,y_sum*z_sum, z_sum);
-	cv::Mat u = (cv::Mat_<double>(3,1)<< z_sum ,x_sum*z_sum , y_sum*z_sum);
-
-	//cv::Mat ml = (cv::Mat_<double>(3,3) << 10,-9,-12,7,-12,11,-10,10,3);
-
-	//std::cout << "ml_inv : " << ml.inv() << std::endl;
 	std::cout << "M : " << M<< std::endl;
 	std::cout << "M_inv : " << M.inv() << std::endl;
 	std::cout << "u : " << u<< std::endl;
 
 	cv::Mat projector_parametter;  
 	projector_parametter = M.inv()*u;
-	//projector_parametter = M.inv()*u;
 
 	std::cout << "projector_parametter : " << projector_parametter << std::endl;
+	std::cout << "projector_parametter.data[0] : " << cv::saturate_cast<double>(projector_parametter.at<double>(0,0) ) << std::endl;
+	//std::cout << "projector_parametter.data[0] : " << cv::saturate_cast<double>(projector_parametter.data[0]) << std::endl;
+
+	//ax+by+cz+d = 0
+	std::cout << "sqrt(4): " << sqrt(4.0) << std::endl;
+	double plane_c = 1/sqrt(projector_parametter.at<double>(0,0)*projector_parametter.at<double>(0,0) + projector_parametter.at<double>(1,0)*projector_parametter.at<double>(1,0) + 1);
+	double plane_a = -projector_parametter.at<double>(0,0)*plane_c;
+	double plane_b = -projector_parametter.at<double>(1,0)*plane_c;
+	double plane_d = -projector_parametter.at<double>(2,0)*plane_c;
+
+
+	std::cout << "plane_a : " << plane_a << std::endl;
+	std::cout << "plane_b : " << plane_b << std::endl;
+	std::cout << "plane_c : " << plane_c << std::endl;
+	std::cout << "plane_d : " << plane_d << std::endl;
 
 	return 0;
 
